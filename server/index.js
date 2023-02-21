d69a4295d75e65d8006438e64ba10d10596a0be7
Ankit Das <75267634+meankitdas@users.noreply.github.com>
2023-02-21 20:23:09 +0530

    feat: openai api and mongodb added

--- server/index.js
+++ server/index.js
@@ -2,20 +2,32 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
 app.get("/", async (req, res) => {
   res.send("Hello World");
 });
 
 const startServer = async () => {
-  app.listen(8080, () => {
-    console.log(`Server is running on port 8080`);
-  });
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => {
+      console.log(`Server is running on port 8080`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
