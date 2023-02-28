3c39ad937e292f8a8da2b78f270284fc92bb3b91
Ankit Das <75267634+meankitdas@users.noreply.github.com>
2023-02-28 23:47:44 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -23,7 +23,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-e-8cr4.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -56,7 +56,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         // alert(form.prompt);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://dall-e-8cr4.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
